02553755a8f418037edc39020c20aa747d54109f
Fernando Barrios <barrioslui02@gmail.com>
2022-01-28 21:43:27 -0600

    Change site them to Dark

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -150,10 +150,9 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            &copy; 2022 HackerWay Studios
+
+    
           </p>
         </div>
       </div>
